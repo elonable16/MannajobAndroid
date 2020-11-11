4a484ac8b3cd7c51032e97a854374ce2dbc97fee
elon <aha15951@gmail.com>
2020-11-11 18:04:32 +0900

    android ui add, 화면 xml 추가함

---
+++ app/src/main/java/com/ateam/mannajob/PopRequestMatch.java
@@ -0,0 +1,14 @@
+package com.ateam.mannajob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopRequestMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_request_match);
+    }
+}
---
+++ app/src/main/java/com/ateam/mannajob/PopReview.java
@@ -0,0 +1,14 @@
+package com.ateam.mannajob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopReview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pop_review);
+    }
+}
